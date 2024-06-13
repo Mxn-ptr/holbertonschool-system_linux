98f14d81f7eb265db5d54a9af680a47e1e91eba9
Mxn-ptr <maxence0@hotmail.fr>
2024-06-13 18:05:38 +0200

    Add super simple command ls

--- main.c
+++ main.c
@@ -4,10 +4,11 @@ int main(int argc, char **argv) {
 	DIR *dir;
 	struct dirent *read;
 
-	dir = opendir('.');
+	dir = opendir(".");
 	while ((read = readdir(dir)) != 0)
 	{
-		printf("%s   ", read->d_name);
+		if (strcmp(read->d_name, ".") != 0 && strcmp(read->d_name, ".."))
+			printf("%s   ", read->d_name);
 	}
 	printf("\n");
 	return (0);
