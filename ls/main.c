1992d29a172f808e9f2b91674b911664bf04782b
Mxn-ptr <maxence0@hotmail.fr>
2024-09-25 13:54:51 +0200

    Fix extra line

--- ls/main.c
+++ ls/main.c
@@ -87,7 +87,7 @@ int main(int argc, char **argv)
 	{
 		for (i = 1; i < nb_args; i++)
 		{
-			if (nb_args > 1)
+			if (i > 1)
 				printf("\n");
 			result = _ls(argv[0], argv[i], nb_args - 1, is_sorting);
 		}
